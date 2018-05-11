e4e13f87fe2323f4699753aee8d0d31a500c2245
gsshch <1729719432@qq.com>
2018-05-11 13:33:50 +0800

    a simple project

---
+++ app/driver/.output/eagle/debug/obj/spi.d
@@ -0,0 +1,23 @@
+.output/eagle/debug/obj/spi.o .output/eagle/debug/obj/spi.d : spi.c ../../app/include/driver/spi.h \
+ ../../app/include/driver/../../../app/include/driver/spi_register.h \
+ ../../app/include/driver/../../../app/include/driver/uart.h \
+ ../../app/include/driver/../../../app/include/driver/../../../app/include/driver/uart_register.h \
+ ../../include/eagle_soc.h ../../include/c_types.h \
+ ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../../include/osapi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/string.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/_ansi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/newlib.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/config.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/machine/ieeefp.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/reent.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/_ansi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/_types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/lock.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ ../../include/os_type.h ../../include/ets_sys.h ../include/user_config.h \
+ ../../include/os_type.h ../../include/spi_flash.h \
+ ../../app/include/driver/spi_overlap.h ../../include/gpio.h \
+ ../../include/user_interface.h ../../include/ip_addr.h \
+ ../../include/queue.h ../../include/../app/include/user_config.h \
+ ../../include/spi_flash.h ../../include/gpio.h ../../include/mem.h
---
+++ app/driver/.output/eagle/debug/obj/spi_overlap.d
@@ -0,0 +1,20 @@
+.output/eagle/debug/obj/spi_overlap.o .output/eagle/debug/obj/spi_overlap.d : spi_overlap.c ../../app/include/driver/spi_overlap.h \
+ ../../include/ets_sys.h ../../include/c_types.h \
+ ../../include/eagle_soc.h ../../include/spi_flash.h \
+ ../../app/include/driver/spi.h \
+ ../../app/include/driver/../../../app/include/driver/spi_register.h \
+ ../../app/include/driver/../../../app/include/driver/uart.h \
+ ../../app/include/driver/../../../app/include/driver/../../../app/include/driver/uart_register.h \
+ ../../include/eagle_soc.h ../../include/c_types.h ../../include/osapi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/string.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/_ansi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/newlib.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/config.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/machine/ieeefp.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/reent.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/_ansi.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/_types.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/sys/lock.h \
+ /opt/xtensa-lx106-elf/lib/gcc/xtensa-lx106-elf/4.8.2/include/stddef.h \
+ ../../include/os_type.h ../../include/ets_sys.h ../include/user_config.h \
+ ../../include/os_type.h ../../include/gpio.h
